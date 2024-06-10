f98bf7c182e3744f4c6b596f4d38d8372edba98b
wtd707934484 <55447488+wtd707934484@users.noreply.github.com>
2024-06-10 14:11:14 +0800

    更新 qqMusic.js

--- backup/qqMusic.js
+++ backup/qqMusic.js
@@ -42,7 +42,7 @@ https://u6.y.qq.com/cgi-bin/musics.fcg? url script-request-header https://raw.gi
 https://raw.githubusercontent.com/wtd707934484/my-quantumult-x/master/getcookie/get_qqMusic_cookie.conf, tag=QQ音乐, update-interval=172800, opt-parser=false, enabled=false
 
 [task_local]
-25 7-12/1 * * * https://raw.githubusercontent.com/WowYiJiu/Personal/main/Script/qqMusic.js, tag=QQ音乐, img-url=https://raw.githubusercontent.com/WowYiJiu/Personal/main/icon/Color/qqMusic.png, enabled=true
+25 7-12/1 * * * https://raw.githubusercontent.com/wtd707934484/my-quantumult-x/master/backup/qqMusic.js, tag=QQ音乐, img-url=https://raw.githubusercontent.com/WowYiJiu/Personal/main/icon/Color/qqMusic.png, enabled=true
 */
 
 const $ = new Env("QQ音乐");
